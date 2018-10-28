5a1d97ecb67a18b02d4a88014d4aeac0b803f7e3
Funketeer <44098088+Funkatier@users.noreply.github.com>
2018-10-28 21:48:51 +0100

    adding all files first time

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$("ul").on("click", "li", function(){ 
+    $(this).toggleClass("checked");
+}); 
+
+$("input[type='text']").keypress(function(event){
+    if( event.which === 13){ 
+        var newTodo = $(this).val(); 
+        $(this).val("");
+        $("ul").append("<li> <span> <i class='fas fa-trash-alt'></i></span> " + newTodo + "</li>");
+    }
+})
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500 , function(){ 
+        $(this).remove();
+    })
+    event.stopPropagation();
+});
+
+$(".fa-pencil-alt").click(function(){
+    $("input[type='text']").fadeToggle(400);
+});
+
+$("li:nth-of-type(4)").css({
+    color: "grey",
+    textDecoration: "line-through",
+})
