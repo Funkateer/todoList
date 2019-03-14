c49a3fb29006c4d8ade361e4dc675434bbc2d912
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-03-14 21:00:48 +0100

    clear all blank spaces and missing semicolons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
-$("ul").on("click", "li", function(){ 
+$("ul").on("click", "li", function(){
     $(this).toggleClass("checked");
-}); 
+});
 
 $("input[type='text']").keypress(function(event){
-    if( event.which === 13){ 
-        var newTodo = $(this).val(); 
+    if( event.which === 13){
+        var newTodo = $(this).val();
         $(this).val("");
         $("ul").append("<li> <span> <i class='fas fa-trash-alt'></i></span> " + newTodo + "</li>");
     }
 });
 
 $("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500 , function(){ 
+    $(this).parent().fadeOut(500 , function(){
         $(this).remove();
-    })
+    });
     event.stopPropagation();
 });
 
