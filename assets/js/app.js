134275481b9ce27c81b43b3837cce7f058fdb863
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-03-30 19:14:17 +0100

    change size of indentation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,27 @@
 $("ul").on("click", "li", function(){
-    $(this).toggleClass("checked");
+	$(this).toggleClass("checked");
 });
 
 $("input[type='text']").keypress(function(event){
-    if( event.which === 13){
-        var newTodo = $(this).val();
-        $(this).val("");
-        $("ul").append("<li> <span> <i class='fas fa-trash-alt'></i></span> " + newTodo + "</li>");
-    }
+	if( event.which === 13){
+		var newTodo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li> <span> <i class='fas fa-trash-alt'></i></span> " + newTodo + "</li>");
+	}
 });
 
 $("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500 , function(){
-        $(this).remove();
-    });
-    event.stopPropagation();
+	$(this).parent().fadeOut(500 , function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
 
 $(".fa-pencil-alt").click(function(){
-    $("input[type='text']").fadeToggle(400);
+	$("input[type='text']").fadeToggle(400);
 });
 
 $("li:nth-of-type(4)").css({
-    color: "grey",
-    textDecoration: "line-through",
+	color: "grey",
+	textDecoration: "line-through",
 });
